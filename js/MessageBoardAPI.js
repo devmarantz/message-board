1153f05f7508e6a72274d564c532e3772fc20080
Devon Marantz <DMarantz@talentpath.com>
2019-03-04 14:29:03 -0800

    Add delete API functions!

--- js/MessageBoardAPI.js
+++ js/MessageBoardAPI.js
@@ -1,7 +1,19 @@
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve,ms));
+}
+
 class MessageBoardAPI {
   constructor(comments = []) {
     this.comments = comments;
   }
+
+  // returns all comments
+  getComments() {
+    // return wait(1000).then(() => this.comments);
+    return fetch('https://marantzexpress.herokuapp.com/api/comments').then(response => response.json());
+  }
+
+
 
   /**
    * Adds a new comment to the comments array
@@ -9,14 +21,27 @@ class MessageBoardAPI {
    * @returns {array} Updated comments array
    */
   addComment(text) {
-    const id = this.comments.length > 0 ? this.comments[this.comments.length - 1].id + 1 : 0;
-    const timestamp = Date.now();
-    this.comments.push({
+    // const id = this.comments.length > 0 ? this.comments[this.comments.length - 1].id + 1 : 0;
+    // const timestamp = Date.now();
+    // this.comments.push({
+    //   text,
+    //   id,
+    //   timestamp,
+    //   likes: 0,
+    //   dislikes: 0
+    // });
+    // return wait(2000).then(() => this.comments);
+    const body = {
+      // text: text
       text,
-      id,
-      timestamp,
-    });
-    return this.comments;
+    };
+    return fetch('https://marantzexpress.herokuapp.com/api/comments', {
+      method: 'POST',
+      headers:  {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }).then(response => response.json());
   }
 
   /**
@@ -36,9 +61,12 @@ class MessageBoardAPI {
    * @returns {array} Updated comments array
    */
   removeComment(id) {
-    const index = this.comments.findIndex(comment => comment.id === id);
-    this.comments.splice(index, 1);
-    return this.comments;
+    // const index = this.comments.findIndex(comment => comment.id === id);
+    // this.comments.splice(index, 1);
+    // return wait(1500).then(() => this.comments);
+    return fetch(`https://marantzexpress.herokuapp.com/api/comments/${id}`, {
+      method: 'DELETE',
+    }).then(response => response.json());
   }
 
   /**
@@ -61,8 +89,19 @@ class MessageBoardAPI {
    * @param {string} substring Substring to be filtered
    * @returns {array} Filtered array of comment objects
    */
-  filterCommentsByText(substring) {
-    return this.comments.filter(comment => comment.text.toLowerCase().includes(substring.toLowerCase()));
+  filterCommentsByText(substring = '') {
+    return wait(1000).then(() => this.comments.filter(comment => comment.text.toLowerCase().includes(substring.toLowerCase()))
+    );
+  }
+
+  likeComment(id) {
+    this.comments.find(comment => comment.id === id).likes++;
+    return this.comments;
+  }
+
+  dislikeComment(id) {
+    this.comments.find(comment => comment.id === id).dislikes++;
+    return this.comments;
   }
 }
 
@@ -74,25 +113,35 @@ export const commentData = [
     text: 'Love this!',
     id: 1,
     timestamp: 1549581565,
+    likes: 0,
+    dislikes: 0
   },
   {
     text: 'Super good',
     id: 2,
     timestamp: 1549577965,
+    likes: 0,
+    dislikes: 0
   },
   {
     text: 'You are the best',
     id: 3,
     timestamp: 1549495165,
+    likes: 0,
+    dislikes: 0
   },
   {
     text: 'Ramen is my fav food ever',
     id: 4,
     timestamp: 1548976765,
+    likes: 0,
+    dislikes: 0
   },
   {
     text: 'Nice Nice Nice!',
     id: 5,
     timestamp: 1546903165,
+    likes: 0,
+    dislikes: 0
   },
 ];
